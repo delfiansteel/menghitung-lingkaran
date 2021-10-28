4733968606afc8ef267d7c9fd2f6896bbe99affe
delfiansteel <delfiansteel7.ds@gmail.com>
2021-10-28 21:38:23 +0700

    Menambahkan Tulisan

---
+++ menghitung.py
@@ -0,0 +1,17 @@
+phi = 3.14
+
+
+r = float(input("Masukkan jari-jari lingkaran: "))
+
+luas = phi*r*r
+keliling = 2*phi*r
+
+
+print ("Rumus luas lingkaran     = ",phi,"x",r,"x",r,)
+print ("Rumus Keliling Lingkaran = ",2,"x",phi,"x",r,)
+
+
+
+print ()
+print ("Hasil luas lingkaran     = " ,luas)
+print ("Hasil Keliling Lingkaran = " ,keliling)      
